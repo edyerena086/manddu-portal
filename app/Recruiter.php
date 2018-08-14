289df8be8c604c3a946e871b8279cc2cbcc68cac
Administrador <edgar.yerena@metodika.mx>
2018-08-14 15:15:33 -0500

    Recruiter initial migrations

---
+++ app/Recruiter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ReclutaTI;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Recruiter extends Model
+{
+    const ADMIN = 1;
+    const USER = 2;
+}
---
+++ database/migrations/2018_08_14_201224_create_recruiters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecruitersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recruiters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('recruiter_company_id');
+            $table->string('last_name')->nullable();
+            $table->integer('account_type')->default(\ReclutaTI\Recruiter::USER);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recruiters');
+    }
+}
